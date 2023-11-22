91ae0e317db2ff701e8e9377a4eb6715c5dc2588
Mike Zamayias <29067825+mikezamayias@users.noreply.github.com>
2023-11-22 21:45:53 +0200

    Remove BlueprintView from followers_view.dart and search_user_view.dart

--- lib/views/followers_view.dart
+++ lib/views/followers_view.dart
@@ -1,14 +1,10 @@
 import 'package:flutter/material.dart';
 
-import '../widgets/blueprint_view.dart';
-
 class FollowersView extends StatelessWidget {
   const FollowersView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BlueprintView(
-      child: Container(),
-    );
+    return Container();
   }
 }
--- lib/views/search_user_view.dart
+++ lib/views/search_user_view.dart
@@ -1,15 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-import '../widgets/blueprint_view.dart';
-
 class SearchUserView extends StatelessWidget {
   const SearchUserView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BlueprintView(
-      child: Container(),
-    );
+    return Container();
   }
 }
