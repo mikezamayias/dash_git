06c16ff2baa68207c9a85cb9f427a50a4807d447
Mike Zamayias <29067825+mikezamayias@users.noreply.github.com>
2023-11-22 12:52:40 +0200

    adds views

---
+++ lib/views/followers_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/blueprint_view.dart';
+
+class FollowersView extends StatelessWidget {
+  const FollowersView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlueprintView(
+      child: Container(),
+    );
+  }
+}
---
+++ lib/views/repositories_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/blueprint_view.dart';
+
+class RepositoriesView extends StatelessWidget {
+  const RepositoriesView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlueprintView(
+      child: Container(),
+    );
+  }
+}
