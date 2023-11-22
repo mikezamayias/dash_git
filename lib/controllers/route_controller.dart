1f88d41fb8284ae0be02ecd1fa1300b2f4fe4c2e
Mike Zamayias <29067825+mikezamayias@users.noreply.github.com>
2023-11-22 12:45:47 +0200

    adds route controlls

---
+++ lib/controllers/route_controller.dart
@@ -0,0 +1,66 @@
+import 'dart:developer';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../models/route_model.dart';
+import '../views/followers_view.dart';
+import '../views/repositories_view.dart';
+import '../views/search_user_view.dart';
+import '../views/user_profile_view.dart';
+
+class RouteController {
+  //   singleton constructor
+  static final RouteController _instance = RouteController._internal();
+
+  factory RouteController() => _instance;
+
+  RouteController._internal();
+
+  //  routes for user profile, repositories, followers, and search
+  static final userProfileRoute = RouteModel(
+    label: 'User Profile',
+    icon: Builder(
+      builder: (context) => Icon(context.platformIcons.person),
+    ),
+    view: const UserProfileView(),
+  );
+  static final repositoriesRoute = RouteModel(
+    label: 'Repositories',
+    icon: Builder(
+      builder: (context) => Icon(context.platformIcons.folder),
+    ),
+    view: const RepositoriesView(),
+  );
+  static final followersRoute = RouteModel(
+    label: 'Followers',
+    icon: Builder(
+      builder: (context) => Icon(context.platformIcons.person),
+    ),
+    view: const FollowersView(),
+  );
+  static final searchRoute = RouteModel(
+    label: 'Search User',
+    icon: Builder(
+      builder: (context) => Icon(context.platformIcons.search),
+    ),
+    view: const SearchUserView(),
+  );
+  final routes = [
+    userProfileRoute,
+    repositoriesRoute,
+    followersRoute,
+    searchRoute,
+  ];
+  static StateProvider currentRouteIndexProvider =
+      StateProvider<int>((ref) => 0);
+  static StateProvider currentRouteProvider = StateProvider<RouteModel>((ref) {
+    final index = ref.watch(currentRouteIndexProvider);
+    log(
+      _instance.routes[index].label,
+      name: 'RouteController.currentRoute',
+    );
+    return _instance.routes[index];
+  });
+}
---
+++ lib/models/route_model.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/widgets.dart';
+
+class RouteModel {
+  final String label;
+  final Widget icon;
+  final Widget view;
+
+  RouteModel({
+    required this.label,
+    required this.icon,
+    required this.view,
+  });
+}
