3f558bb4d03eec12f67d62cd1f5539d8d67e3d17
Mike Zamayias <29067825+mikezamayias@users.noreply.github.com>
2023-11-24 11:59:45 +0200

    Refactor widget: ListWithHeader to ListWithTitleAndSubtitle

---
+++ lib/widgets/list_with_title_and_subtitle.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+
+class ListWithTitleAndSubtitle extends StatelessWidget {
+  const ListWithTitleAndSubtitle({
+    super.key,
+    this.title,
+    required this.header,
+    required this.widgets,
+  });
+
+  final Widget? title;
+  final Widget header;
+  final List<Widget> widgets;
+
+  @override
+  Widget build(BuildContext context) {
+    return PlatformWidget(
+      cupertino: (context, _) => SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              alignment: Alignment.center,
+              padding: const EdgeInsets.only(top: 16),
+              child: header,
+            ),
+            CupertinoListSection.insetGrouped(
+              topMargin: 0,
+              hasLeading: false,
+              header: title,
+              children: widgets,
+            ),
+          ],
+        ),
+      ),
+      material: (context, _) => SingleChildScrollView(
+        primary: true,
+        child: Column(
+          children: [
+            header,
+            if (title != null) title!,
+            ListView.separated(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              itemBuilder: (context, index) => widgets[index],
+              separatorBuilder: (context, index) => const Divider(),
+              itemCount: widgets.length,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
